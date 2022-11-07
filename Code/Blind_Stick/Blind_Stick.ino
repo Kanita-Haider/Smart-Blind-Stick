9b66db7aa18c51d2409ed72a072a987d2c9874da
Kanita-Haider <kanitahaider44@gmail.com>
2022-11-07 20:09:23 +0600

    Create Blind_Stick.ino

---
+++ Code/Blind_Stick/Blind_Stick.ino
@@ -0,0 +1,41 @@
+const int trigPin = 3;
+const int echoPin = 2;
+const int buzzer = 5;
+const int motorPin = 6;
+
+long duration;
+int distance;
+int safetyDistance;
+void setup() 
+{
+pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(buzzer, OUTPUT);
+pinMode(motorPin, OUTPUT);
+Serial.begin(9600); 
+}
+
+void loop()
+ {
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+safetyDistance = distance;
+
+if (safetyDistance <= 30)
+{ 
+ digitalWrite(buzzer, HIGH);  
+digitalWrite(motorPin, HIGH);
+}
+else
+{
+  digitalWrite(buzzer, LOW);
+  digitalWrite(motorPin, LOW);
+}
+Serial.print("Distance: ");
+Serial.println(distance);
+}
